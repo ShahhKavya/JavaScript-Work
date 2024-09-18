a8f1656a3c92a77d4e9154ba93d4bdaa49c6b2ce
Kavya Shah <kavyashah918@gmail.com>
2024-09-18 19:16:24 +0530

    Add files via upload

---
+++ js/variable.js
@@ -0,0 +1,14 @@
+const enrollNo=127
+let studentEmail="kavyashah918@gmail.com"
+var password="12345"
+//var can't be used as  scope problem is there 
+studentCity="Rajkot" // this is a bad habit without using variable 
+
+// enrollNo=126 CONST CANT BE CHANGED 
+studentEmail="kavya"
+password="367899"
+studentCity="Ahmedabad"
+let studentMarks
+
+console.table([enrollNo,studentEmail,password,studentCity,studentMarks]);
+
