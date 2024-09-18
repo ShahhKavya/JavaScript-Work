f526c351d6f21c5a83f20550ceb28b2d6d3638ef
Kavya Shah <kavyashah918@gmail.com>
2024-09-18 20:33:37 +0530

    Add files via upload

---
+++ js/03_dataType_conversion.js
@@ -0,0 +1,15 @@
+// let marks="83.5abc";
+// console.log(typeof marks);
+
+// let score=Number(marks);
+// console.log(typeof score);
+// console.log(score);
+
+
+// let response=" "
+// let bool=Boolean(response);
+// console.log(bool);
+
+let name="kavya"
+let no=Number(name);
+console.log(no)
