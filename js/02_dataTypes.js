59e8eeb71b92a6e4d04bdb2b62371f195da4fa72
Kavya Shah <kavyashah918@gmail.com>
2024-09-18 19:59:36 +0530

    Add files via upload

---
+++ js/02_dataTypes.js
@@ -0,0 +1,16 @@
+// number
+//bigint
+let kavya=123;
+//string
+
+//symbol for unique things 
+
+//boolean
+
+//null standalone value 
+//undefined
+
+// object
+
+console.log(typeof kavya)
+console.table([typeof null,typeof undefined])
